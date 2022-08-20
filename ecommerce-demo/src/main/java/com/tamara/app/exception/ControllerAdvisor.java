a567846d43edbd7be6e2bb2c6abdc3ba71d28c3c
Aravind <piratlaaru@gmail.com>
2022-08-20 13:59:18 +0400

    Update ControllerAdvisor.java
    Updated response for Error Structure

--- ecommerce-demo/src/main/java/com/tamara/app/exception/ControllerAdvisor.java
+++ ecommerce-demo/src/main/java/com/tamara/app/exception/ControllerAdvisor.java
@@ -1,13 +1,18 @@
 package com.tamara.app.exception;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.validation.ConstraintViolationException;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -38,5 +43,26 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
       return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
     
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+
+        //Get all errors
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(x -> x.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, headers, status);
+
+    }
     
 }
