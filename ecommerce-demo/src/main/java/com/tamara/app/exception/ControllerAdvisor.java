471f823aeaa1ca6bf8ecc8e74688bcd9b8f14515
Aravind <piratlaaru@gmail.com>
2022-08-20 15:51:29 +0400

    Error message Changes

--- ecommerce-demo/src/main/java/com/tamara/app/exception/ControllerAdvisor.java
+++ ecommerce-demo/src/main/java/com/tamara/app/exception/ControllerAdvisor.java
@@ -27,7 +27,7 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
-        body.put("message", "Order Not found");
+        body.put("message", ex.getMessage());
 
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
